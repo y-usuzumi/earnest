f31c43b3d4cb305d433e587fca6d82b529366a92
Kenneth Zhao <kenneth.zhao@netis.com.cn>
2018-09-20 11:11:01 +0800

    MonadIO and MonadThrow instances for Transaction

--- src/Earnest/Transaction.hs
+++ src/Earnest/Transaction.hs
@@ -6,18 +6,21 @@ import           Control.Monad
 import           Control.Monad.Catch
 import           Control.Monad.IO.Class
 
-newtype Transaction r = Transaction { action :: forall n. MonadThrow n => n r
+newtype Transaction r = Transaction { action :: forall m n. (MonadIO m, MonadThrow n) => m (n r)
                                     } deriving Functor
 
 instance Applicative Transaction where
-  pure a = Transaction $ return a
+  pure a = Transaction $ return $ return a
   (<*>) = ap
 
 instance Monad Transaction where
-  Transaction action >>= f = Transaction $ do
-    action >>= \r -> do
-      let Transaction next = f r
-      next
+  Transaction action >>= f = join $ fmap (join . fmap f) action
+
+instance MonadIO Transaction where
+  liftIO io = Transaction $ liftIO $ fmap return io
+
+instance MonadThrow Transaction where
+  throwM e = Transaction $ return $ throwM e
 
 data CreateOrderException deriving Show
 
