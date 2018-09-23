ec3d93c6f1530ab1ba4e182b9abe5a5093271397
Yukio Usuzumi <anohigisavay@gmail.com>
2018-09-23 21:35:31 +0800

    loadInitialInfo

--- src/Earnest/Exchange.hs
+++ src/Earnest/Exchange.hs
@@ -3,16 +3,17 @@ module Earnest.Exchange where
 import           Control.Monad.Catch
 import           Control.Monad.IO.Class
 import           Control.Monad.State
-import qualified Data.HashMap.Strict    as HM
-import qualified Data.Set               as S
+import qualified Data.HashMap.Strict           as HM
+import qualified Data.Set                      as S
 import           Earnest.Currency
+import           Earnest.Exchange.CurrencyPair
 import           Earnest.Order
 import           Earnest.Protocol
 import           Earnest.Transaction
 
 
 class Exchange e where
-  type Protocol e :: *
+  loadInitialInfo :: MonadIO m => e -> m ExchangeInfo
 
-data ExchangeState = ExchangeState { supportedExchanges :: String
-                                   }
+data ExchangeInfo = ExchangeInfo { supportedCurrencyPairs :: ExchangePairLookup
+                                 }
--- src/Earnest/Exchange/ServiceProviders/GateHub.hs
+++ src/Earnest/Exchange/ServiceProviders/GateHub.hs
@@ -1,1 +1,17 @@
 module Earnest.Exchange.ServiceProviders.GateHub where
+
+import           Control.Monad.State
+import           Earnest.Currency
+import           Earnest.Exchange
+import           Earnest.Exchange.CurrencyPair
+
+data GateHubExchange = GateHubExchange
+
+instance Exchange GateHubExchange where
+  loadInitialInfo _ = do
+    let currencyPairs = flip execState newExchangePairLookup $ do
+          merge [ (CNY, XRP)
+                , (XRP, CNY)
+                ]
+    return ExchangeInfo{ supportedCurrencyPairs = currencyPairs
+                       }
