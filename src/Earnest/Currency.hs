42abea7c36f68c66e13412ae3f8bea8a182e1b70
Yukio Usuzumi <anohigisavay@gmail.com>
2018-09-27 01:19:26 +0800

    All currencies

--- src/Earnest/Currency.hs
+++ src/Earnest/Currency.hs
@@ -1,12 +1,88 @@
 module Earnest.Currency where
 
-import GHC.Generics (Generic)
 import           Data.Hashable
+import           GHC.Generics  (Generic)
 
-data Currency = CNY
-              | USD
+data Currency = AE
+              | ARDR
+              | ATN
+              | BASH
+              | BCC
+              | BCD
+              | BCV
+              | BCX
+              | BITE
+              | BitCNY
+              | BKBT
+              | BLK
+              | BNT
               | BTC
+              | BTM
+              | BTS
+              | CAN
+              | CDR
+              | CNC   -- specific to AEX
+              | CNET
+              | CNY
+              | CVC
+              | DASH
+              | DAT
+              | DOGE
+              | EAC
+              | ELA
+              | EOS
+              | EOSDAC
+              | ETC
+              | ETH
+              | FGC
+              | GNX
+              | GOD
+              | HLB
+              | IDT
+              | INF
+              | KNC
+              | LTC
+              | LXT
+              | LBTC
+              | LEND
+              | LV
+              | MEC
+              | MGC
+              | NCS
+              | NULX
+              | NXT
+              | IGNIS
+              | JRC
+              | OCT
+              | OMG
+              | OPC
+              | QASH
+              | QRK
+              | SAC
+              | SBTC
+              | SEER
+              | SNT
+              | STB
+              | SYS
+              | TAG
+              | TMC
+              | TYT
+              | UBTC
+              | USD
+              | USDT  -- specific to AEX
+              | VASH
+              | VNS
+              | WIC
+              | XAS
+              | XCN
+              | XEM
+              | XLM
+              | XPM
               | XRP
+              | XYT
+              | XZC
+              | YOYO
+              | ZCC
               deriving (Eq, Generic, Read, Show)
 
 instance Hashable Currency
