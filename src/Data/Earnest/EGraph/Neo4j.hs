0f4cef788955aaff9a5a1d3a9d3089045ad2ae38
Yukio Usuzumi <anohigisavay@gmail.com>
2018-10-23 00:44:02 +0800

    Same thing, but much faster

--- src/Data/Earnest/EGraph/Neo4j.hs
+++ src/Data/Earnest/EGraph/Neo4j.hs
@@ -19,60 +19,47 @@ import           Prelude                          hiding (last)
 type Q p = forall m. MonadIO m => B.Pipe -> p -> m [B.Record]
 type Q_ p = forall m. MonadIO m => B.Pipe -> p -> m ()
 
-qMerge :: Q_ (UUID, (Currency, HBourse), (Currency, HBourse), TradeInfo)
-qMerge p (gid, (fc, fb), (tc, tb), ti) = do
-  liftIO $ putStrLn [i|Merging #{fc} -> #{tc}|]
-  mergeNode p gid fc fb
-  mergeNode p gid tc tb
-  mergeRelation p gid fc fb tc tb
+qMerge :: Q_ [(UUID, (Currency, HBourse), (Currency, HBourse), TradeInfo)]
+qMerge p ls = liftIO $ do
+  let params = flip map ls $ \(gid, (fc, fb), (tc, tb), ti) ->
+        B.M $ M.fromList [ ("gid", B.T $ T.pack $ show gid)
+                         , ("fc", B.T $ T.pack $ show fc)
+                         , ("fb", B.T $ T.pack $ show fb)
+                         , ("tc", B.T $ T.pack $ show tc)
+                         , ("tb", B.T $ T.pack $ show tb)
+                         , ("fee", B.F $ fee ti)
+                         , ("buy", B.F $ buy ti)
+                         , ("sell", B.F $ sell ti)
+                         , ("high", B.F $ high ti)
+                         , ("low", B.F $ fee ti)
+                         , ("last", B.F $ last ti)
+                         , ("vol", B.F $ vol ti)
+                         ]
+  B.run p $ B.queryP_ stmt $ M.singleton "l" (B.L params)
   where
-    mergeNodeStmt = [i|
-                      MERGE (:Currency {
-                      gid: $gid,
-                      c: $c,
-                      b: $b
-                      })
-                      |]
-    mergeRelationStmt = [i|
-                          MATCH (f:Currency {
-                          gid: $gid,
-                          c: $fc,
-                          b: $fb
-                          }), (t:Currency {
-                          gid: $gid,
-                          c: $tc,
-                          b: $tb
-                          })
-                          MERGE (f)-[:TradeInfo{
-                            f:$fee,
-                            b:$buy,
-                            s:$sell,
-                            h:$high,
-                            l:$low,
-                            p:$last,
-                            v:$vol,
-                            d:[]
-                            }]->(t)
-                          |]
-    mergeNode p gid c b =
-      B.run p $ B.queryP_ mergeNodeStmt $ M.fromList [ ("gid", B.T $ T.pack $ show gid)
-                                                     , ("c", B.T $ T.pack $ show c)
-                                                     , ("b", B.T $ T.pack $ show b)
-                                                     ]
-    mergeRelation p gid fc fb tc tb =
-      B.run p $ B.queryP_ mergeRelationStmt $ M.fromList [ ("gid", B.T $ T.pack $ show gid)
-                                                         , ("fc", B.T $ T.pack $ show fc)
-                                                         , ("fb", B.T $ T.pack $ show fb)
-                                                         , ("tc", B.T $ T.pack $ show tc)
-                                                         , ("tb", B.T $ T.pack $ show tb)
-                                                         , ("fee", B.F $ fee ti)
-                                                         , ("buy", B.F $ buy ti)
-                                                         , ("sell", B.F $ sell ti)
-                                                         , ("high", B.F $ high ti)
-                                                         , ("low", B.F $ low ti)
-                                                         , ("last", B.F $ last ti)
-                                                         , ("vol", B.F $ vol ti)
-                                                         ]
+    stmt = [i|
+             UNWIND $l AS pair
+             MERGE (m:Currency {
+             gid: pair.gid,
+             c: pair.fc,
+             b: pair.fb
+             })
+             MERGE (n:Currency {
+             gid: pair.gid,
+             c: pair.tc,
+             b: pair.tb
+             })
+             MERGE (m)-[:TradeInfo{
+               f:pair.fee,
+               b:pair.buy,
+               s:pair.sell,
+               h:pair.high,
+               l:pair.low,
+               p:pair.last,
+               v:pair.vol,
+               d:[]
+               }]->(n)
+             |]
 
 newtype Neo4jGraph = Neo4jGraph UUID
                    deriving Show
@@ -85,8 +72,7 @@ instance EGraph Neo4jGraph where
     bis <- mapM (\(HBourse b) -> loadInfo b) bourses
     forM_ (zip bourses bis) $ \(b, bi) -> do
       let ls = TI.toList $ bi ^. supportedTrades
-      forM_ ls $ \(f, t, ti) -> do
-        qMerge driver (gid, (f, b), (t, b), ti)
+      qMerge driver $ map (\(f, t, ti) -> (gid, (f, b), (t, b), ti)) ls
     return $ Neo4jGraph gid
 
   getTradableOptions = undefined
